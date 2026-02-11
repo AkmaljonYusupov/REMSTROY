e3819bfc859f3dce88c23adda05a01cf91091e6c
akmaljonyusupov <infoakmaljon.uz@gmail.com>
2026-02-11 16:03:06 +0500

    about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from 'react-i18next'
 import aboutCartimg1 from '../assets/icons/card1-icon.png'
 import aboutCartimg2 from '../assets/icons/card2-icon.png'
 import aboutCartimg3 from '../assets/icons/card3-icon.png'
-
+// import aboutpageImg from '../src/assets/images/aboutpage-image.jpg'
 import './About.scss'
 
 function About() {
@@ -62,46 +62,55 @@ function About() {
 			</div>
 
 			{/* ============================================= */}
-			{/* RASHMDAGI "We Believe In The Power of Great Ideas" BO'LIMI – 1:1 mos */}
+			{/* RASMDAGI "About Us" BO'LIMI – chapda rasm, o'ngda kontent (eng yaqin moslashuv) */}
 			{/* ============================================= */}
-			<div className='about-believe-section'>
+			<div className='about-us-hero'>
 				<div className='container'>
-					<div className='believe-content'>
-						<h2 className='believe-title'>
-							{t('about.believe.title')}{' '}
-							{/* "We Believe In The Power of Great Ideas" */}
-						</h2>
+					<div className='us-grid'>
+						{/* Chap taraf: Rasm + overlay effekt */}
+						<div className='us-image-side'>
+							<img
+								src='/src/assets/images/aboutpage-image.jpg' // <-- bu rasmni o'zgartiring (masalan image:3 yoki image:6 dan oling)
+								alt='Construction team discussing plans'
+								className='us-main-photo'
+							/>
+						</div>
 
-						<p className='believe-desc'>
-							{t('about.believe.desc')} {/* lorem ipsum o'rniga real matn */}
-						</p>
+						{/* O'ng taraf: Matn bloki */}
+						<div className='us-text-side'>
+							<span className='us-small-label'>{t('nav.reviews')}</span>
 
-						<ul className='believe-checklist'>
-							<li>
-								<span className='check-icon'>✔</span>
-								{t('about.believe.check1')}
-							</li>
-							<li>
-								<span className='check-icon'>✔</span>
-								{t('about.believe.check2')}
-							</li>
-							<li>
-								<span className='check-icon'>✔</span>
-								{t('about.believe.check3')}
-							</li>
-							<li>
-								<span className='check-icon'>✔</span>
-								{t('about.believe.check4')}
-							</li>
-							<li>
-								<span className='check-icon'>✔</span>
-								{t('about.believe.check5')}
-							</li>
-						</ul>
+							<h2 className='us-main-title'>{t('about.believe.title')}</h2>
 
-						<button className='btn-read-more'>
-							{t('about.believe.readMore')}
-						</button>
+							<p className='us-description'>{t('about.believe.desc')}</p>
+
+							<ul className='us-check-list'>
+								<li>
+									<span className='check-mark'>✔</span>
+									{t('about.believe.check1')}
+								</li>
+								<li>
+									<span className='check-mark'>✔</span>
+									{t('about.believe.check2')}
+								</li>
+								<li>
+									<span className='check-mark'>✔</span>
+									{t('about.believe.check3')}
+								</li>
+								<li>
+									<span className='check-mark'>✔</span>
+									{t('about.believe.check4')}
+								</li>
+								<li>
+									<span className='check-mark'>✔</span>
+									{t('about.believe.check5')}
+								</li>
+							</ul>
+
+							<button className='read-more-btn'>
+								{t('about.believe.readMore')}
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
