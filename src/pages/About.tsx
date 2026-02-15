d8d052edad10784804b8f8eef968024b5e4fcda9
akmaljonyusupov <infoakmaljon.uz@gmail.com>
2026-02-15 07:20:02 +0500

    about section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -340,6 +340,24 @@ function About() {
 				</div>
 			</div>
 
+			{/* MAP SECTION */}
+			<div className='about-map'>
+
+
+				<div className='map-frame'>
+					<iframe
+						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.835434509374!2d144.95592831567338!3d-37.81720917975195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad65d43f1f5f5b1%3A0x5045675218ce6e0!2s180%20Flinders%20St%2C%20Melbourne%20VIC%203000%2C%20Australia!5e0!3m2!1sen!2sau!4v1707949500000!5m2!1sen!2sau"
+						width="100%"
+						height="550"
+						style={{ border: 0 }}
+						allowFullScreen=""
+						loading="lazy"
+						referrerPolicy="no-referrer-when-downgrade"
+						title="Melbourne Office Location"
+					></iframe>
+				</div>
+			</div>
+
 
 
 		</section>
