ed7aa6a02966f43ba77820b9bfae3bcb97fe54a5
akmaljonyusupov <infoakmaljon.uz@gmail.com>
2026-02-15 07:30:02 +0500

    about section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -346,7 +346,7 @@ function About() {
 
 				<div className='map-frame'>
 					<iframe
-						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.835434509374!2d144.95592831567338!3d-37.81720917975195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad65d43f1f5f5b1%3A0x5045675218ce6e0!2s180%20Flinders%20St%2C%20Melbourne%20VIC%203000%2C%20Australia!5e0!3m2!1sen!2sau!4v1707949500000!5m2!1sen!2sau"
+						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2996.893932426892!2d69.33107757572047!3d41.31117100071977!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38aef50327e60f73%3A0x671d676293ffbc9!2z0YPQu9C40YbQsCDQn9Cw0YDQutC10L3RgtGB0LrQsNGPLCDQotCw0YjQutC10L3RgiwgVGFzaGtlbnQsINCj0LfQsdC10LrQuNGB0YLQsNC9!5e0!3m2!1sru!2s!4v1771122502240!5m2!1sru!2s"
 						width="100%"
 						height="450"
 						style={{ border: 0 }}
@@ -355,6 +355,8 @@ function About() {
 						referrerPolicy="no-referrer-when-downgrade"
 						title="Melbourne Office Location"
 					></iframe>
+
+
 				</div>
 			</div>
 
