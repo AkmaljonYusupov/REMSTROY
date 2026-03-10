e96c166af9bd59159ccbf6cbd3f8690045681ba2
akmaljonyusupov <infoakmaljon.uz@gmail.com>
2026-03-10 05:58:52 +0500

    add galreya page

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -2,11 +2,22 @@ import { useEffect, useRef, useState } from "react"
 import { useTranslation } from "react-i18next"
 import "./Gallery.scss"
 
+import img6 from "../assets/images/projectImg01.jpg"
+import img10 from "../assets/images/projectImg010.png"
+import img13 from "../assets/images/projectImg0110.jpg"
+import img11 from "../assets/images/projectImg0110.png"
+import img15 from "../assets/images/projectImg0120.jpg"
 import img5 from "../assets/images/projectImg02.png"
 import img4 from "../assets/images/projectImg03.png"
 import img3 from "../assets/images/projectImg04.png"
 import img2 from "../assets/images/projectImg05.png"
 import img1 from "../assets/images/projectImg06.png"
+import img7 from "../assets/images/projectImg07.png"
+import img8 from "../assets/images/projectImg08.png"
+import img9 from "../assets/images/projectImg09.png"
+import img14 from "../assets/images/projectImg110.jpg"
+import img12 from "../assets/images/split_image_1.png"
+import img16 from "../assets/images/split_image_2.png"
 import heroImage from "../assets/images/sub-banner-img.jpg"
 
 type ImageType = {
@@ -26,7 +37,18 @@ export default function Gallery() {
     { id: 3, src: img3 },
     { id: 4, src: img4 },
     { id: 5, src: img5 },
-  ];
+    { id: 6, src: img6 },
+    { id: 7, src: img7 },
+    { id: 8, src: img8 },
+    { id: 9, src: img9 },
+    { id: 10, src: img10 },
+    { id: 11, src: img11 },
+    { id: 13, src: img13 },
+    { id: 14, src: img14 },
+    { id: 15, src: img15 },
+    { id: 16, src: img16 },
+    { id: 12, src: img12 }
+ ]
 
   const imageTitles = t("gallery.images", { returnObjects: true }) as string[];
 
